ae19a78279d684d63548fb9b88c3d1ed5a1da455
Itobuz <itobuz@Itobuzs-MacBook-Pro.local>
2024-03-12 12:57:11 +0530

    complete: Faithful hearts, unleash potential

--- home-figma/index.js
+++ home-figma/index.js
@@ -1,10 +1,24 @@
 import navbarComponent from "../navbar/navbar.js";
 import footerComponent from "../footer/footer.js";
-import callPriceComponent from "./call-price-section/callPrice.js";
 
 const navbarSection = document.querySelector('.navbar-section');
 const footerSection = document.querySelector('.footer-section');
-const callPriceSection = document.querySelector('.call-price-section');
 navbarSection.innerHTML = navbarComponent();
 footerSection.innerHTML = footerComponent();
-callPriceSection.append(callPriceComponent());
+
+function satisfactionCard(imageURL, imageName, title, paragraph) {
+    const container = document.createElement('div');
+    const innerContainer = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const image = document.createElement('img');
+    image.setAttribute('src', imageURL);
+    image.setAttribute('alt', imageName);
+    imageContainer.append(image);
+    const heading = document.createElement('h4');
+    heading.innerText = title;
+    const content = document.createElement('p');
+    content.innerText = paragraph;
+    const readMoreButton = document.createElement('div');
+    const readMore  = document.createElement('p');
+    readMore.innerText = 'Read More';
+}
